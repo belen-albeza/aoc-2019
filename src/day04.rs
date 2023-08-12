e208afcea213c8f044b97f67835b00c87d6e8593
Belén Albeza <belen@hey.com>
2023-08-12 22:32:54 +0200

    solve day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -39,6 +39,39 @@ fn password_gen(start: u64, end: u64) -> PasswordGen {
     PasswordGen { curr: start, end }
 }
 
+struct RepeatingChunker {
+    i: usize,
+    digits: Vec<u64>,
+}
+
+impl Iterator for RepeatingChunker {
+    type Item = Vec<u64>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i >= self.digits.len() {
+            return None;
+        }
+
+        let value = self.digits[self.i];
+
+        let repeating: Vec<u64> = self.digits[self.i..]
+            .iter()
+            .take_while(|x| **x == value)
+            .map(|x| *x)
+            .collect();
+
+        self.i += repeating.len();
+        Some(repeating)
+    }
+}
+
+fn repeating_chunks(digits: &[u64]) -> RepeatingChunker {
+    RepeatingChunker {
+        i: 0,
+        digits: digits.to_owned(),
+    }
+}
+
 fn is_valid_password(password: &[u64]) -> bool {
     let has_valid_length = password.len() == 6;
     let has_double = password.windows(2).any(|pair| pair[0] == pair[1]);
@@ -47,12 +80,30 @@ fn is_valid_password(password: &[u64]) -> bool {
     has_double && is_not_decreasing && has_valid_length
 }
 
+fn is_valid_password_v2(password: &[u64]) -> bool {
+    let has_valid_length = password.len() == 6;
+    let is_not_decreasing = password.windows(2).all(|pair| pair[0] <= pair[1]);
+
+    let has_isolated_double = repeating_chunks(password).any(|x| x.len() == 2);
+
+    has_valid_length && is_not_decreasing && has_isolated_double
+}
+
 #[aoc(day4, part1)]
 pub fn solve_part1(input: &(u64, u64)) -> u64 {
     let (start, end) = *input;
 
     password_gen(start, end)
         .filter(|x| is_valid_password(x))
+        .count() as u64
+}
+
+#[aoc(day4, part2)]
+pub fn solve_part2(input: &(u64, u64)) -> u64 {
+    let (start, end) = *input;
+
+    password_gen(start, end)
+        .filter(|x| is_valid_password_v2(x))
         .count() as u64
 }
 
@@ -65,5 +116,18 @@ mod tests {
         assert_eq!(is_valid_password(&[1, 1, 1, 1, 1, 1]), true);
         assert_eq!(is_valid_password(&[2, 2, 3, 4, 5, 0]), false);
         assert_eq!(is_valid_password(&[1, 2, 3, 7, 8, 9]), false);
+    }
+
+    #[test]
+    fn test_is_valid_password_v2() {
+        assert_eq!(is_valid_password_v2(&[1, 1, 2, 2, 3, 3]), true);
+        assert_eq!(is_valid_password_v2(&[1, 2, 3, 4, 4, 4]), false);
+        assert_eq!(is_valid_password_v2(&[1, 1, 1, 1, 2, 2]), true);
+        assert_eq!(is_valid_password_v2(&[1, 1, 1, 1, 1, 1]), false);
+        assert_eq!(is_valid_password_v2(&[4, 4, 5, 6, 7, 8]), true);
+        assert_eq!(is_valid_password_v2(&[4, 5, 6, 8, 8, 9]), true);
+        assert_eq!(is_valid_password_v2(&[4, 4, 5, 6, 7, 8]), true);
+        assert_eq!(is_valid_password_v2(&[4, 5, 5, 6, 7, 8]), true);
+        assert_eq!(is_valid_password_v2(&[8, 8, 8, 9, 9, 9]), false);
     }
 }
