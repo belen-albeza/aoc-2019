09c131600ed5aaf24cb7595234b4d150e52280d7
Belén Albeza <belen@hey.com>
2023-08-13 20:17:36 +0200

    day 5 part 1 (wip - input + output)

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,6 @@
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
+use std::io;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum ParamMode {
@@ -65,6 +66,9 @@ impl Instruction {
     pub fn modes2(&self) -> [ParamMode; 3] {
         [self.modes[0], self.modes[1], ParamMode::Immediate]
     }
+    pub fn modes1(&self) -> [ParamMode; 3] {
+        [self.modes[0], ParamMode::Immediate, ParamMode::Immediate]
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -81,15 +85,23 @@ impl VM {
         }
     }
 
-    pub fn run(&mut self) -> Result<i64, String> {
+    pub fn run(
+        &mut self,
+        input: &mut impl io::BufRead,
+        output: &mut impl io::Write,
+    ) -> Result<i64, String> {
         while let Some(raw_instruction) = self.memory.get(self.ip) {
             let instruction = Instruction::try_from(*raw_instruction)?;
+            println!("Running {:?}", instruction);
             match instruction.opcode {
                 Opcode::Add => self.exec_add(instruction.modes2())?,
                 Opcode::Mul => self.exec_mul(instruction.modes2())?,
+                Opcode::Input => self.exec_input(input, instruction.modes1())?,
+                Opcode::Output => self.exec_output(output, instruction.modes1())?,
                 Opcode::Halt => break,
                 _ => todo!("unimplemented"),
             }
+            println!("\t-> {:?}", self.memory);
         }
 
         Ok(*self.memory.get(0).unwrap_or(&0))
@@ -119,13 +131,23 @@ impl VM {
             self.read_mem(self.ip + 3)?,
         ];
 
-        for i in 0..modes.len() {
+        for i in 0..params.len() {
             if modes[i] == ParamMode::Position {
                 params[i] = self.read_mem(params[i] as usize)?;
             }
         }
 
         Ok((params[0], params[1], params[2]))
+    }
+
+    fn read_params1(&mut self, modes: [ParamMode; 3]) -> Result<i64, String> {
+        let mut param = self.read_mem(self.ip + 1)?;
+
+        if modes[0] == ParamMode::Position {
+            param = self.read_mem(param as usize)?;
+        }
+
+        Ok(param)
     }
 
     fn exec_add(&mut self, modes: [ParamMode; 3]) -> Result<(), String> {
@@ -147,11 +169,44 @@ impl VM {
         self.ip += 4;
         Ok(())
     }
-}
 
-fn run(src: &[i64]) -> i64 {
-    let mut vm = VM::new(src);
-    vm.run().unwrap()
+    fn exec_output(
+        &mut self,
+        output: &mut impl io::Write,
+        modes: [ParamMode; 3],
+    ) -> Result<(), String> {
+        let x = self.read_params1(modes)?;
+        writeln!(output, "{}", x).map_err(|x| format!("{}", x))?;
+
+        self.ip += 2;
+        Ok(())
+    }
+
+    fn exec_input(
+        &mut self,
+        input: &mut impl io::BufRead,
+        modes: [ParamMode; 3],
+    ) -> Result<(), String> {
+        let mut buffer = String::new();
+        input
+            .read_to_string(&mut buffer)
+            .map_err(|x| format!("{}", x))?;
+        println!("buffer = {}", buffer);
+
+        let value: i64 = buffer
+            .parse()
+            .map_err(|_| format!("invalid input: {}", buffer))?;
+        let x = self.read_params1([
+            ParamMode::Immediate,
+            ParamMode::Position,
+            ParamMode::Position,
+        ])?;
+
+        self.write_mem(x as usize, value)?;
+
+        self.ip += 2;
+        Ok(())
+    }
 }
 
 #[aoc_generator(day5)]
@@ -162,6 +217,20 @@ pub fn parse_input(input: &str) -> Vec<i64> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn run(src: &[i64] /* input: &mut impl io::BufRe, output: &mut impl io::Write */) -> i64 {
+        // io::stdin.lock()
+        // io::stdout()
+        run_with_buffers(src, "", &mut vec![])
+
+        // let mut vm = VM::new(src);
+        // vm.run(&input.as_bytes(), &mut output).unwrap()
+    }
+
+    fn run_with_buffers(src: &[i64], input: &str, output: &mut impl io::Write) -> i64 {
+        let mut vm = VM::new(src);
+        vm.run(&mut input.as_bytes(), output).unwrap()
+    }
 
     #[test]
     fn test_parse_input() {
@@ -201,5 +270,21 @@ mod tests {
     #[test]
     fn test_run_with_modes() {
         assert_eq!(run(&vec![1101, 100, -1, 0]), 99);
+    }
+
+    #[test]
+    fn test_output_in_buffer() {
+        let mut buffer = vec![];
+        run_with_buffers(&vec![4, 0], "", &mut buffer);
+        assert_eq!(String::from_utf8(buffer).unwrap(), "4\n");
+    }
+
+    #[test]
+    fn test_input_in_buffer() {
+        let mut buffer = vec![];
+        assert_eq!(
+            run_with_buffers(&vec![3, 2, 0, 1, 1, 0], "1101", &mut buffer),
+            2
+        );
     }
 }
