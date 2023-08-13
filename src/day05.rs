efd0612f6cdf742986c78bee94de85d72f9e6ddb
Belén Albeza <belen@hey.com>
2023-08-13 17:56:59 +0200

    day 5 part 1 (wip - implement modes)

--- src/day05.rs
+++ src/day05.rs
@@ -2,6 +2,23 @@ use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
+enum ParamMode {
+    Position,
+    Immediate,
+}
+
+impl TryFrom<i64> for ParamMode {
+    type Error = String;
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Position),
+            1 => Ok(Self::Immediate),
+            _ => Err(format!("unrecognized param mode: {}", value)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Opcode {
     Add,
     Mul,
@@ -20,6 +37,32 @@ impl TryFrom<i64> for Opcode {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Copy)]
+struct Instruction {
+    opcode: Opcode,
+    modes: [ParamMode; 3],
+}
+
+impl TryFrom<i64> for Instruction {
+    type Error = String;
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        let opcode = Opcode::try_from(value % 100)?;
+        let mut modes = [ParamMode::Position; 3];
+        for i in 0..modes.len() {
+            let raw_mode = (value % 10_i64.pow(3 + i as u32)) / 10_i64.pow(2 + i as u32);
+            modes[i] = ParamMode::try_from(raw_mode)?;
+        }
+
+        Ok(Self { opcode, modes })
+    }
+}
+
+impl Instruction {
+    pub fn modes2(&self) -> [ParamMode; 3] {
+        [self.modes[0], self.modes[1], ParamMode::Immediate]
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 struct VM {
     ip: usize,
@@ -35,11 +78,11 @@ impl VM {
     }
 
     pub fn run(&mut self) -> Result<i64, String> {
-        while let Some(raw_opcode) = self.memory.get(self.ip) {
-            let opcode = Opcode::try_from(*raw_opcode)?;
-            match opcode {
-                Opcode::Add => self.exec_add()?,
-                Opcode::Mul => self.exec_mul()?,
+        while let Some(raw_instruction) = self.memory.get(self.ip) {
+            let instruction = Instruction::try_from(*raw_instruction)?;
+            match instruction.opcode {
+                Opcode::Add => self.exec_add(instruction.modes2())?,
+                Opcode::Mul => self.exec_mul(instruction.modes2())?,
                 Opcode::Halt => break,
                 // _ => todo!("unimplemented"),
             }
@@ -65,29 +108,37 @@ impl VM {
         Ok(())
     }
 
-    fn read_params3(&mut self) -> Result<(i64, i64, i64), String> {
-        let x = self.read_mem(self.ip + 1)?;
-        let y = self.read_mem(self.ip + 2)?;
-        let z = self.read_mem(self.ip + 3)?;
+    fn read_params3(&mut self, modes: [ParamMode; 3]) -> Result<(i64, i64, i64), String> {
+        let mut params = [
+            self.read_mem(self.ip + 1)?,
+            self.read_mem(self.ip + 2)?,
+            self.read_mem(self.ip + 3)?,
+        ];
 
-        Ok((x, y, z))
+        for i in 0..modes.len() {
+            if modes[i] == ParamMode::Position {
+                params[i] = self.read_mem(params[i] as usize)?;
+            }
+        }
+
+        Ok((params[0], params[1], params[2]))
     }
 
-    fn exec_add(&mut self) -> Result<(), String> {
-        let (ix, iy, iz) = self.read_params3()?;
+    fn exec_add(&mut self, modes: [ParamMode; 3]) -> Result<(), String> {
+        let (x, y, z) = self.read_params3(modes)?;
 
-        let sum = self.read_mem(ix as usize)? + self.read_mem(iy as usize)?;
-        self.write_mem(iz as usize, sum)?;
+        let sum = x + y;
+        self.write_mem(z as usize, sum)?;
 
         self.ip += 4;
         Ok(())
     }
 
-    fn exec_mul(&mut self) -> Result<(), String> {
-        let (ix, iy, iz) = self.read_params3()?;
+    fn exec_mul(&mut self, modes: [ParamMode; 3]) -> Result<(), String> {
+        let (x, y, z) = self.read_params3(modes)?;
 
-        let mul = self.read_mem(ix as usize)? * self.read_mem(iy as usize)?;
-        self.write_mem(iz as usize, mul)?;
+        let mul = x * y;
+        self.write_mem(z as usize, mul)?;
 
         self.ip += 4;
         Ok(())
@@ -121,10 +172,30 @@ mod tests {
     }
 
     #[test]
-    fn test_run_simple() {
+    fn test_parse_instruction() {
+        assert_eq!(
+            Instruction::try_from(1002),
+            Ok(Instruction {
+                opcode: Opcode::Mul,
+                modes: [
+                    ParamMode::Position,
+                    ParamMode::Immediate,
+                    ParamMode::Position
+                ]
+            })
+        );
+    }
+
+    #[test]
+    fn test_run_backward_compatibility() {
         assert_eq!(run(&vec![1, 0, 0, 0, 99]), 2);
         assert_eq!(run(&vec![2, 3, 0, 3, 99]), 2);
         assert_eq!(run(&vec![2, 4, 4, 5, 99, 0]), 2);
         assert_eq!(run(&vec![1, 1, 1, 4, 99, 5, 6, 0, 99]), 30);
+    }
+
+    #[test]
+    fn test_run_with_modes() {
+        assert_eq!(run(&vec![1101, 100, -1, 0]), 99);
     }
 }
